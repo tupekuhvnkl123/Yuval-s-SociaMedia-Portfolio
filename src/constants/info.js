3eeb2a5a7daa54da153559cee9ccffdb05072dba
ofek levi <ofeklevi36@gmail.com>
2024-05-03 16:08:57 +0300

    fix

--- src/constants/info.js
+++ src/constants/info.js
@@ -1,14 +1,6 @@
 import heroImage from "../assets/Hero.jpg";
 import aboutImage from "../assets/AboutMe.jpg";
 import whyImage from "../assets/Why.png";
-import animals1Poster from "../assets/animals1.png";
-import animals2Poster from "../assets/animals2.png";
-import animals3Poster from "../assets/animals3.png";
-import bookPoster from "../assets/book.png";
-import laGoffrePoster from "../assets/laGoffre.png";
-import meatILPoster from "../assets/meatIL.png";
-import wedding1Poster from "../assets/wedding1.png";
-import wedding2Poster from "../assets/wedding2.png";
 
 export const heroInfo = {
   name: "Yuval Cohen",
@@ -28,19 +20,22 @@ export const examplesFirstSectionInfo = [
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/mgeasmybfec5ykaxugdh",
     title: "Animals I",
-    poster: animals1Poster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/animals1",
   },
   {
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/nm9jz3pr5r8upqkhaae8",
     title: "La Goffre",
-    poster: laGoffrePoster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/laGoffre",
   },
   {
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/jz9iep8bizj8op79alhv",
     title: "Wedding I",
-    poster: wedding1Poster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/wedding1",
   },
 ];
 export const examplesSecondSectionInfo = [
@@ -48,31 +43,36 @@ export const examplesSecondSectionInfo = [
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/norppmra6xhta0aoxpac",
     title: "Animals II",
-    poster: animals2Poster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/animals2",
   },
   {
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/fngwsdlnnniqcyufwpgj",
     title: "Wedding II",
-    poster: wedding2Poster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/wedding2",
   },
   {
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/tm5tn8ikb3plfhgxmapf",
     title: "Meat IL",
-    poster: meatILPoster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/meatIL",
   },
   {
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/eln7almhpqmgoxztodya",
     title: "Animals III",
-    poster: animals3Poster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/animals3",
   },
   {
     srcUrl:
       "https://res.cloudinary.com/dnxqlkwvr/video/upload/f_auto:video,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/uxgyes2eiizcigz8qd8p",
     title: "Book",
-    poster: bookPoster,
+    poster:
+      "https://res.cloudinary.com/dnxqlkwvr/image/upload/f_auto,q_auto/v1/Yuval's%20SocialMedia%20Portfolio/book",
   },
 ];
 
